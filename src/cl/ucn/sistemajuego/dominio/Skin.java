56ea9e9a99256fe271141a892ec07d3290031da3
Pablo <90540501+PaizoV@users.noreply.github.com>
2021-10-15 17:33:27 -0300

    Creado el objeto Skin

---
+++ src/cl/ucn/sistemajuego/dominio/Skin.java
@@ -0,0 +1,38 @@
+package cl.ucn.sistemajuego.dominio;
+
+public class Skin {
+	private String nombre;
+	private String calidad;
+	private int precio;
+	
+	public Skin(String nombre, String calidad, int precio) {
+		this.nombre = nombre;
+		this.calidad = calidad;
+		this.precio = precio;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCalidad() {
+		return calidad;
+	}
+
+	public void setCalidad(String calidad) {
+		this.calidad = calidad;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+}
